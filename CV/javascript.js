5e19c1ec076ca4eb948d36dde4546574c7ad4e2f
gonzaleosuarez <94195105+gonzaleosuarez@users.noreply.github.com>
2023-01-30 21:10:16 -0300

    Add files via upload

---
+++ CV/javascript.js
@@ -0,0 +1,36 @@
+function verMas() {
+    document.getElementById('mas').style.display = 'block';
+}
+
+function ocultar() {
+    document.getElementById('mas').style.display = 'none';
+}
+
+function masInfo() {
+    document.getElementById('ver').style.display = 'block';
+}
+
+function menos() {
+    document.getElementById('ver').style.display = 'none';
+}
+
+function verMasinfo() {
+    document.getElementById('masInfo').style.display = 'block';
+}
+
+function ocultarInfo() {
+    document.getElementById('masInfo').style.display = 'none';
+}
+
+function verHabilidades() {
+    document.getElementById('habilidades').style.display = 'block';
+}
+
+function ocultarHabilidades() {
+    document.getElementById('habilidades').style.display = 'none';
+}
+
+
+
+
+
